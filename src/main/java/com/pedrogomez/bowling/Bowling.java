d82745b4f0fef57e5914db8840f9e1a2d709b191
pedrovgs <pedrovicente.gomez@gmail.com>
2014-03-01 20:14:33 +0100

    Create main class and main integration tests.

---
+++ src/main/java/com/pedrogomez/bowling/Bowling.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 Pedro Vicente Gómez Sánchez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pedrogomez.bowling;
+
+/**
+ * Main class of Bowling kata. This class has the responsibility of instrument all the application dependencies and
+ * create a facade to offer the implementation using a single access point. This class will get a string parameter
+ * with a valid sequence of rolls for one line and will return an integer with the score.
+ *
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class Bowling {
+
+    /**
+     * Calculate the score of a roll sequence for a bowling match.
+     *
+     * @param rolls valid sequence of rolls in one line.
+     * @return calculated score.
+     */
+    public int getScore(String rolls) {
+        return -1;
+    }
+
+}
--- src/test/java/com/pedrogomez/bowling/BowlingTest.java
+++ src/test/java/com/pedrogomez/bowling/BowlingTest.java
@@ -15,6 +15,14 @@
  */
 package com.pedrogomez.bowling;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Test created to check the correctness of Bowling class. This test contains integration tests to check the score
  * has been calculated properly.
@@ -22,4 +30,56 @@ package com.pedrogomez.bowling;
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class BowlingTest {
+
+    /*
+     * Test data
+     */
+
+    private Bowling bowling;
+
+    private Map<String, Integer> validRollsSequences;
+
+    /*
+     * Before and after methods
+     */
+
+    @Before
+    public void setUp() {
+        initializeBowling();
+        initializeRollsSequences();
+    }
+
+
+
+    /*
+     * Test methods
+     */
+
+    @Test
+    public void shouldCalculateTheCorrectScore() {
+        for (String key : validRollsSequences.keySet()) {
+            int result = bowling.getScore(key);
+            int expectedResult = validRollsSequences.get(key);
+
+            assertEquals(expectedResult, result);
+        }
+    }
+
+    /*
+     * Auxiliary methods
+     */
+
+    private void initializeBowling() {
+        bowling = new Bowling();
+    }
+
+    private void initializeRollsSequences() {
+        validRollsSequences = new HashMap<String, Integer>();
+        validRollsSequences.put("XXXXXXXXXXXX", 300);
+        validRollsSequences.put("9-9-9-9-9-9-9-9-9-9-", 90);
+        validRollsSequences.put("5/5/5/5/5/5/5/5/5/5/5", 150);
+        validRollsSequences.put("238143XX5/--189/XXX", 135);
+        validRollsSequences.put("8-8-8-8-8-8-8-8-8-8-", 80);
+    }
+
 }
